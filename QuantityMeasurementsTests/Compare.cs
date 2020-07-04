040902499477915b208339a2f5cec9b75e8bcacf
Abhishek Bodake <rishibodake@gmail.com>
2020-07-04 13:18:21 +0530

    Added:CMethod To Compare The Lengths

---
+++ QuantityMeasurementsTests/Compare.cs
@@ -0,0 +1,14 @@
+﻿namespace QuantityMeasurementsTests
+{
+    public class Compare
+    {
+        public bool Check(int feets, int inches)
+        {
+            if(inches/feets == inches)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
