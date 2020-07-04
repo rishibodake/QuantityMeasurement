7c99fb3534108f6f13988cc53e57bd9bcdf92976
Abhishek Bodake <rishibodake@gmail.com>
2020-07-04 20:22:38 +0530

    Added:

--- QuantityMeasurements/AllUnits.cs
+++ QuantityMeasurements/AllUnits.cs
@@ -16,21 +16,31 @@ namespace QuantityMeasurements
                     case null:
                         throw new CustomException(CustomException.TypeOfException.NULL_POINTER_EXCEPTION);
                     case "feet":
-                        return Math.Round(value * 30.38);
+                        return value * 12;
                     case "yards":
-                        return Math.Round(value * 91.44);
-                    case "inches":
-                        return Math.Round(value * 2.54);
+                        return value * 36;
                     case "cm":
+                        return value/2.5;
+                    case "inches":
                         return value;
                     default:
                         throw new CustomException(CustomException.TypeOfException.CHECK_TYPE_OF_VALUE);
                 }
             }
-            catch (CustomException e)
+            catch (CustomException)
             {
                 throw new CustomException(CustomException.TypeOfException.NULL_POINTER_EXCEPTION);
             }
-        } 
+        }
+
+        public static bool Addition(double value_1, double value_2, double expected_value)
+        {
+            double result = value_1 + value_2;           
+            if (result == expected_value)
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }
