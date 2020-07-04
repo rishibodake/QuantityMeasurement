c0d5d50b6cc481b10c8d9996e7bdb893bcdc27c6
Abhishek Bodake <rishibodake@gmail.com>
2020-07-04 15:02:13 +0530

    Added:Methods To Convert the lengths between

---
+++ QuantityMeasurements/Conversion.cs
@@ -0,0 +1,15 @@
+﻿namespace QuantityMeasurements
+{
+    public class Conversion
+    {
+        public double FeetsToInches(double feet)
+        {
+            return feet * 12;
+        }
+
+        public double InchesToFeets(double inches)
+        {
+            return inches / 12;
+        }
+    }
+}
