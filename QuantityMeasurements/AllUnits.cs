92aaf1e333a08057cc51ca24f9c84f64ea0a09d8
Abhishek Bodake <rishibodake@gmail.com>
2020-07-04 17:00:05 +0530

    Added:Method To Generically Convert All The Units into Base Unit Inches

---
+++ QuantityMeasurements/AllUnits.cs
@@ -0,0 +1,31 @@
+﻿
+
+using System.Reflection.Metadata.Ecma335;
+
+namespace QuantityMeasurements
+{
+    public static class AllUnits
+    {
+       public static double ConvertToInches(double value,string unit)
+        {
+            try
+            {
+                switch (unit.ToLower())
+                {
+                    case null:
+                        throw new CustomException(CustomException.TypeOfException.NULL_POINTER_EXCEPTION);
+                    case "feet":
+                        return value * 12;
+                    case "yards":
+                        return value * 39;
+                    default:
+                        throw new CustomException(CustomException.TypeOfException.CHECK_TYPE_OF_VALUE);
+                }
+            }
+            catch (CustomException e)
+            {
+                throw new CustomException(CustomException.TypeOfException.NULL_POINTER_EXCEPTION);
+            }
+        } 
+    }
+}
---
+++ QuantityMeasurements/CustomException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace QuantityMeasurements
+{
+    public class CustomException : Exception
+    {
+        public enum TypeOfException
+        {
+            NULL_POINTER_EXCEPTION,CHECK_TYPE_OF_VALUE
+        }
+        public TypeOfException typeOfException;
+
+        public CustomException(TypeOfException typeOfException)
+        {
+            this.typeOfException = typeOfException;
+        }
+    }
+
+
+}
