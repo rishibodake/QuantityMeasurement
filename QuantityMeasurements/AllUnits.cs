865761867e54d89cb00abf366d9206e8fbcf5403
Abhishek Bodake <rishibodake@gmail.com>
2020-07-04 19:36:56 +0530

    Added:Method To Add 2 diffrent Quantities

--- QuantityMeasurements/AllUnits.cs
+++ QuantityMeasurements/AllUnits.cs
@@ -31,8 +31,16 @@ namespace QuantityMeasurements
             {
                 throw new CustomException(CustomException.TypeOfException.NULL_POINTER_EXCEPTION);
             }
-        } 
+        }
 
-       
+        public static bool Addition(double value_1, double value_2, double expected_value)
+        {
+            double result = value_1 + value_2;
+            if (result == expected_value)
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }
